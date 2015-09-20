fc8c1175d5dae5540b2e50aa42ce1eba88a8159a
Poachman <chrispoach@gmail.com>
2015-09-20 15:47:53 -0700

    made fallingPlanes interesting

--- fallingPlanes.cpp
+++ fallingPlanes.cpp
@@ -6,23 +6,30 @@ void fallingPlanes::doFrame() {
   int speed = 1;
   this->cube.drawBackground(Color(RGBv(0,0,0)));
 
-  if(this->frame / speed % 60 < 30) {
+  if(this->frame / speed % 90 < 30) {
     if(this->frame / speed % 30 < 15) {
       for(int z = 0; z < 16; z++)
-        this->cube.drawLine(Point(0,0,z), Point(this->frame / speed % 15,15,z), c);
+        this->cube.drawLine(Point(0,0,z), Point(this->frame / speed % 15, 15, z), c);
     } else {
       for(int z = 0; z < 16; z++)
         this->cube.drawLine(Point(0,0,z), Point(15, 15 - this->frame / speed % 15, z), c);
     }
+  } else if(this->frame / speed % 90 < 60) {
+    if(this->frame / speed % 30 < 15) {
+      for(int z = 0; z < 16; z++)
+        this->cube.drawLine(Point(z,0,0), Point(z, this->frame / speed % 15, 15), c);
+    } else {
+      for(int z = 0; z < 16; z++)
+        this->cube.drawLine(Point(z,0,0), Point(z, 15, 15 - this->frame / speed % 15), c);
+    }
   } else {
     if(this->frame / speed % 30 < 15) {
       for(int z = 0; z < 16; z++)
-        this->cube.drawLine(Point(0,0,z), Point(15,this->frame / speed % 15,z), c);
+        this->cube.drawLine(Point(0,z,0), Point(15, z, this->frame / speed % 15), c);
     } else {
       for(int z = 0; z < 16; z++)
-        this->cube.drawLine(Point(0,0,z), Point(15 - this->frame / speed % 15, 15, z), c);
+        this->cube.drawLine(Point(0,z,0), Point(15 - this->frame / speed % 15, z, 15), c);
     }
   }
-
   this->frame++;
 }
